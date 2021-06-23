af894ddddd12f5527b8a4b96d41d0cba01356ddc
Fyodor <feodor.1243@gmail.com>
2021-06-23 19:29:39 +0700

    dump_all_functions.py

---
+++ dump_all_functions.py
@@ -0,0 +1,25 @@
+import random
+
+
+def more(x, y):
+    return x+y
+
+
+def less():
+    numbers = [12312, 14312342134, 234, 213, 1, 1231234234, 2134321, 3241, 234]
+    return random.choice(numbers)
+
+
+def test_in():
+    intervals = ['big_septima''big_septima', 'big_seconda', 'small_seconda', 'clean_prima', 'clean_octava', 'clean_quarta', 'clean_quinta', 'big_tertia', 'small_tertia', 'big_sexta', 'small_sexta']
+    return random.choice(intervals)
+
+
+def truth(s):
+    b = s.lower()
+    return b
+
+
+def equal():
+    Rurics = {'Olgerd', 'Svyatoslav', 'Svyatopolk', 'Ingvar', 'Mstyslav'}
+    return Rurics
