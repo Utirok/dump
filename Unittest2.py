ece1dc923aa980c96f5192cab0e77530e049100d
Fyodor <feodor.1243@gmail.com>
2021-06-23 19:25:59 +0700

    Unittest2.py

--- Unittest2.py
+++ Unittest2.py
@@ -2,7 +2,6 @@ import random
 import unittest
 import numpy as numpy
 
-
 class Main(unittest.TestCase):
     def test_random(self):
         fractions = numpy.random.random(50)
@@ -10,7 +9,6 @@ class Main(unittest.TestCase):
         for fraction in fractions:
             with self.subTest(x=fraction):
                 self.assertGreaterEqual(fraction, 0.5)
-
 
 if __name__ == '__main__':
     unittest.main()
