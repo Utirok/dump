2ad4915747812c66636cad1e29035557f02b4f30
Фёдор Безуглов <f.bezuglov@g.nsu.ru>
2021-07-01 06:49:55 +0700

    tasktime3(again).py

--- tasktime3.py
+++ tasktime3.py
@@ -3,9 +3,9 @@ now = time.localtime()
 today = time.strftime("%d.%m.%Y", now).split('.')
 birthdate = input('date of birth: ').split('.')
 days_number = 0
-if int(birthdate[0]) in range(1, 31):
-    if int(birthdate[1]) in range(1, 12):
-        if int(birthdate[2]) in range(1, 2021):
+if int(birthdate[0]) in range(1, 32):
+    if int(birthdate[1]) in range(1, 13):
+        if int(birthdate[2]) in range(1, 2022):
             date = time.strftime("%d.%m.%Y", now)
             days = int(today[0]) - int(birthdate[0])
             month = (int(today[1]) - int(birthdate[1])) * 30
